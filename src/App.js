3f40adf0177c7635595fcd545cea69033ac9d8db
bpinazmul18 <bpinazmul18@gmail.com>
2022-02-22 23:41:01 +0600

    First commit.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/*
+* Title:- App
+* Description:- Main app file
+* Author:- bpinazmul18
+* */
+
+
+function App() {
+  return (
+    <div>hello</div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+/*
+* Title:- App
+* Description:- Main app file
+* Author:- bpinazmul18
+* */
+
+
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
